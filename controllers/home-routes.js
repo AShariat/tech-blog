41e54ec02c0c97a994d6a95afcb04523e5fbc417
Amir Shariat <amirshariat1987@gmail.com>
2022-04-11 20:55:24 -0600

    homepage created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,34 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // The data that Sequelize returns is actually a Sequelize object with a lot more information attached to it than you might have been expecting. To serialize the object down to only the properties we need, we use Sequelize's get() method.
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
